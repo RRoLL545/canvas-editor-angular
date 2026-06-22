8f8cf10e4e60d4dacc2b85b432aa35b034eae62c
Роман Быков <motom@yandex.ru>
2026-06-22 19:50:36 +0400

    feat: router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet],
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('canvas-editor');
-}
+export class App {}
